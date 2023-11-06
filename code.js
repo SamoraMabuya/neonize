bb845efcd35d9506e984809070fff1618f14fea2
samora <samora.sm.sam@gmail.com>
2023-11-06 22:11:02 +0200

    feature: drop shaodw efefct added

--- code.js
+++ code.js
@@ -1,45 +1,55 @@
 "use strict";
 figma.showUI(__html__);
 figma.ui.resize(400, 400);
-const updateBlurEffect = (msg) => {
-    const { value, } = msg;
+let cloneNode = null;
+const updateBrightness = (msg) => {
+    const { value } = msg;
     // Check if there's a selection on the current page
     if (figma.currentPage.selection.length > 0) {
         // Loop through selected nodes
-        for (const node of figma.currentPage.selection) {
-            // Check if the selected node is a type that can have effects (e.g., 'RECTANGLE', 'ELLIPSE', 'POLYGON')
-            if (node.type === "RECTANGLE" ||
-                node.type === "ELLIPSE" ||
-                node.type === "POLYGON") {
-                // Create a blur effect with the specified radius
-                const blurEffect = {
-                    type: "LAYER_BLUR",
-                    radius: value * 1,
-                    visible: true,
-                };
-                const innerShadow = {
-                    type: "INNER_SHADOW",
-                    radius: value * 1,
-                    visible: true,
-                    offset: {
-                        x: 0,
-                        y: 4,
-                    },
-                    blendMode: "NORMAL",
-                    color: {
-                        r: 0.255,
-                        g: 0.255,
-                        b: 0.255,
-                        a: 1
-                    }
-                };
-                const dropShadow, {};
-                // Set the node's effects to the created blur effect
-                node.effects = [innerShadow];
-                // Log a message to indicate that the blur effect has been applied
-                console.log(`Applied blur effect to ${node.name || "unnamed node"}.`);
+        if (!cloneNode) {
+            for (const node of figma.currentPage.selection) {
+                // Check if the selected node is a type that can have effects (e.g., 'RECTANGLE', 'ELLIPSE', 'POLYGON')
+                if (node.type === "RECTANGLE" ||
+                    node.type === "ELLIPSE" ||
+                    node.type === "POLYGON") {
+                    // Create a blur effect with the specified radius
+                    const blurEffect = {
+                        type: "LAYER_BLUR",
+                        radius: value * 1,
+                        visible: true,
+                    };
+                    const innerShadow = {
+                        type: "INNER_SHADOW",
+                        radius: value * 1,
+                        visible: true,
+                        offset: {
+                            x: 0,
+                            y: 4,
+                        },
+                        blendMode: "NORMAL",
+                        color: { r: 1, g: 1, b: 1, a: 1 },
+                    };
+                    const dropShadow = {
+                        type: "DROP_SHADOW",
+                        color: { r: 1, g: 1, b: 1, a: 1 },
+                        offset: {
+                            x: 0,
+                            y: 4,
+                        },
+                        radius: value * 1.5,
+                        spread: 0,
+                        visible: true,
+                        blendMode: "NORMAL",
+                        showShadowBehindNode: true,
+                    };
+                    node.effects = [innerShadow, dropShadow];
+                    // Set the node's effects to the created blur effect
+                    // Log a message to indicate that the blur effect has been applied
+                    console.log(`Applied blur effect to ${node.name || "unnamed node"}.`);
+                }
             }
         }
     }
 };
-figma.ui.onmessage = updateBlurEffect;
+figma.ui.onmessage = updateBrightness;
--- code.ts
+++ code.ts
@@ -1,23 +1,26 @@
 figma.showUI(__html__);
 figma.ui.resize(400, 400);
 
-const updateBlurEffect = (msg: any) => {
-  const { value,  } = msg;
+let cloneNode: BaseNode | null = null;
+
+const updateBrightness = (msg: any) => {
+  const { value } = msg;
 
   // Check if there's a selection on the current page
   if (figma.currentPage.selection.length > 0) {
     // Loop through selected nodes
-    for (const node of figma.currentPage.selection) {
-      // Check if the selected node is a type that can have effects (e.g., 'RECTANGLE', 'ELLIPSE', 'POLYGON')
-      if (
-        node.type === "RECTANGLE" ||
-        node.type === "ELLIPSE" ||
-        node.type === "POLYGON"
-      ) {
+    if (!cloneNode) {
+      for (const node of figma.currentPage.selection) {
+        // Check if the selected node is a type that can have effects (e.g., 'RECTANGLE', 'ELLIPSE', 'POLYGON')
+        if (
+          node.type === "RECTANGLE" ||
+          node.type === "ELLIPSE" ||
+          node.type === "POLYGON"
+        ) {
           // Create a blur effect with the specified radius
           const blurEffect: BlurEffect = {
             type: "LAYER_BLUR",
-            radius: value * 1  ,// Adjust the value to your desired range
+            radius: value * 1, // Adjust the value to your desired range
             visible: true,
           };
 
@@ -30,33 +33,36 @@ const updateBlurEffect = (msg: any) => {
               y: 4,
             },
             blendMode: "NORMAL",
-            color: {
-              r: 0.255,
-              g: 0.255,
-              b: 0.255,
-              a: 1
-            }
+            color: { r: 1, g: 1, b: 1, a: 1 },
+          };
 
+          const dropShadow: DropShadowEffect = {
+            type: "DROP_SHADOW",
+            color: { r: 1, g: 1, b: 1, a: 1 },
+            offset: {
+              x: 0,
+              y: 4,
+            },
+            radius: value * 1.5,
+            spread: 0,
+            visible: true,
+            blendMode: "NORMAL",
+            showShadowBehindNode: true,
+          };
 
-          }
+  
 
+          node.effects = [innerShadow, dropShadow];
 
-          const dropShadow: DropShadowEffect {
-            
-          }
-
-          
 
           // Set the node's effects to the created blur effect
-          node.effects = [innerShadow];
 
           // Log a message to indicate that the blur effect has been applied
           console.log(`Applied blur effect to ${node.name || "unnamed node"}.`);
         }
       }
     }
-    
+  }
 };
 
-figma.ui.onmessage = updateBlurEffect;
-
+figma.ui.onmessage = updateBrightness;
