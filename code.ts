7d267db1e02eb63e175c45250aaeac9e9ba88e5f
samora <samora.sm.sam@gmail.com>
2023-11-11 15:43:53 +0200

    refactor: house cleaning chore: update tsconfig

--- code.ts
+++ code.ts
@@ -1,61 +1,54 @@
 figma.showUI(__html__);
 figma.ui.resize(400, 400);
 
-let cloneNode: BaseNode | null = null;
+type NodeTypes = "RECTANGLE" | "ELLIPSE" | "POLYGON";
+
+const isValidShapeType = (nodeType: string): NodeTypes => nodeType as NodeTypes;
+
+function CreateEffects(value: number) {
+  const baseGlow: DropShadowEffect = {
+    type: "DROP_SHADOW",
+    color: { r: 1, g: 1, b: 1, a: 1 },
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    radius: value * 0.2, // Adjust the value to your desired range
+    spread: 0,
+    visible: true,
+    blendMode: "NORMAL",
+    showShadowBehindNode: false,
+  };
+
+  const spreadGlow: DropShadowEffect = {
+    type: "DROP_SHADOW",
+    color: { r: 1, g: 1, b: 1, a: 1 },
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    radius: value * 0.8, // Adjust the value to your desired range
+    spread: 11,
+    visible: true,
+    blendMode: "NORMAL",
+    showShadowBehindNode: false,
+  };
+
+  return {
+    baseGlow,
+    spreadGlow,
+  };
+}
 
 figma.ui.onmessage = (messages) => {
   const { value } = messages;
+  const { baseGlow, spreadGlow } = CreateEffects(value);
 
-  // Check if there's a selection on the current page
-  if (figma.currentPage.selection.length > 0) {
-    // Loop through selected nodes
-    if (!cloneNode) {
-      for (const node of figma.currentPage.selection) {
-        // Check if the selected node is a type that can have effects (e.g., 'RECTANGLE', 'ELLIPSE', 'POLYGON')
-        if (
-          node.type === "RECTANGLE" ||
-          node.type === "ELLIPSE" ||
-          node.type === "POLYGON"
-        ) {
-          // Clone the selected node
-
-          const baseGlow: DropShadowEffect = {
-            type: "DROP_SHADOW",
-            color: { r: 1, g: 1, b: 1, a: 1 },
-            offset: {
-              x: 0,
-              y: 0,
-            },
-            radius: value * 0.2, // Adjust the value to your desired range
-            spread: 0,
-            visible: true,
-            blendMode: "NORMAL",
-            showShadowBehindNode: false,
-          };
-
-          const spreadGlow: DropShadowEffect = {
-            type: "DROP_SHADOW",
-            color: { r: 1, g: 1, b: 1, a: 1 },
-            offset: {
-              x: 0,
-              y: 0,
-            },
-            radius: value * 0.8, // Adjust the value to your desired range
-            spread: 11,
-            visible: true,
-            blendMode: "NORMAL",
-            showShadowBehindNode: false,
-          };
-
-          node.effects = [baseGlow, spreadGlow];
-
-          // set position
-
-          // Group the original node and its clone
-
-          // Log a message to indicate that the blur effect has been applied
-        }
-      }
+  for (const shape of figma.currentPage.selection) {
+    const thisType = isValidShapeType(shape.type);
+    if (shape.type === thisType) {
+      shape.effects = [baseGlow, spreadGlow];
     }
   }
+  return null;
 };
