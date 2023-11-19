2122180bee53157c82ef7cfa7c83fd8d4fa88e6d
samora <samora.sm.sam@gmail.com>
2023-11-19 17:05:02 +0200

    feature: add glow effects to text nodes

--- code.js
+++ code.js
@@ -1,14 +1,21 @@
 "use strict";
 figma.showUI(__html__);
 figma.ui.resize(400, 400);
-const shapeValues = ["RECTANGLE", "ELLIPSE", "POLYGON", "TEXT"];
+const shapeValues = ["RECTANGLE", "ELLIPSE", "POLYGON"];
+let cloneNode = null; // Move the cloneNode outside the event handler
 const isValidShapeType = (nodeType) => {
     if (shapeValues.includes(nodeType)) {
         return nodeType;
     }
     return null;
 };
-function CreateEffects(value) {
+const isValidTextType = (nodeType) => {
+    if (nodeType === "TEXT") {
+        return nodeType;
+    }
+    return null;
+};
+function ApplyShapeGlow(value) {
     const baseGlow = {
         type: "DROP_SHADOW",
         color: { r: 1, g: 1, b: 1, a: 1 },
@@ -35,18 +42,39 @@ function CreateEffects(value) {
         blendMode: "NORMAL",
         showShadowBehindNode: false,
     };
+    const textGlow = {
+        type: "LAYER_BLUR",
+        radius: value * 2.8,
+        visible: true,
+    };
     return {
         baseGlow,
         spreadGlow,
+        textGlow,
     };
 }
 figma.ui.onmessage = (messages) => {
     const { value } = messages;
-    const { baseGlow, spreadGlow } = CreateEffects(value);
-    for (const shape of figma.currentPage.selection) {
-        const thisType = isValidShapeType(shape.type);
-        if (shape.type === thisType) {
-            shape.effects = [baseGlow, spreadGlow];
+    const { baseGlow, spreadGlow, textGlow } = ApplyShapeGlow(value);
+    // Check if cloneNode is not null before cloning again
+    if (!cloneNode) {
+        for (const node of figma.currentPage.selection) {
+            const shapeType = isValidShapeType(node.type);
+            const textType = isValidTextType(node.type);
+            if (node.type === shapeType) {
+                node.effects = [baseGlow, spreadGlow];
+            }
+            if (node.type === "TEXT") {
+                cloneNode = node.clone();
+                cloneNode.name = "Clone";
+                node.name = "Base";
+                figma.currentPage.appendChild(cloneNode);
+                const group = figma.group([node, cloneNode], figma.currentPage);
+                group.name = "Group Node With Effect";
+                node.effects = [textGlow];
+                cloneNode.x = node.x;
+                cloneNode.y = node.y;
+            }
         }
     }
     return null;
--- code.ts
+++ code.ts
@@ -1,9 +1,12 @@
 figma.showUI(__html__);
 figma.ui.resize(400, 400);
 
-const shapeValues = ["RECTANGLE", "ELLIPSE", "POLYGON", "TEXT"];
+const shapeValues = ["RECTANGLE", "ELLIPSE", "POLYGON"];
 
-type NodeTypes = "RECTANGLE" | "ELLIPSE" | "POLYGON" | "TEXT";
+type NodeTypes = "RECTANGLE" | "ELLIPSE" | "POLYGON";
+type TextType = "TEXT";
+
+let cloneNode: BaseNode | null = null; // Move the cloneNode outside the event handler
 
 const isValidShapeType = (nodeType: string): NodeTypes | null => {
   if (shapeValues.includes(nodeType)) {
@@ -12,7 +15,14 @@ const isValidShapeType = (nodeType: string): NodeTypes | null => {
   return null;
 };
 
-function CreateEffects(value: number) {
+const isValidTextType = (nodeType: string): TextType | null => {
+  if (nodeType === "TEXT") {
+    return nodeType as TextType;
+  }
+  return null;
+};
+
+function ApplyShapeGlow(value: number) {
   const baseGlow: DropShadowEffect = {
     type: "DROP_SHADOW",
     color: { r: 1, g: 1, b: 1, a: 1 },
@@ -20,7 +30,7 @@ function CreateEffects(value: number) {
       x: 0,
       y: 0,
     },
-    radius: value * 0.2, // Adjust the value to your desired range
+    radius: value * 0.2,
     spread: 0,
     visible: true,
     blendMode: "NORMAL",
@@ -34,28 +44,51 @@ function CreateEffects(value: number) {
       x: 0,
       y: 0,
     },
-    radius: value * 0.8, // Adjust the value to your desired range
+    radius: value * 0.8,
     spread: 11,
     visible: true,
     blendMode: "NORMAL",
     showShadowBehindNode: false,
   };
+  const textGlow: BlurEffect = {
+    type: "LAYER_BLUR",
+
+    radius: value * 2.8,
+    visible: true,
+  };
 
   return {
     baseGlow,
     spreadGlow,
+    textGlow,
   };
 }
 
 figma.ui.onmessage = (messages) => {
   const { value } = messages;
-  const { baseGlow, spreadGlow } = CreateEffects(value);
+  const { baseGlow, spreadGlow, textGlow } = ApplyShapeGlow(value);
 
-  for (const shape of figma.currentPage.selection) {
-    const thisType = isValidShapeType(shape.type);
-    if (shape.type === thisType) {
-      shape.effects = [baseGlow, spreadGlow];
+  // Check if cloneNode is not null before cloning again
+  if (!cloneNode) {
+    for (const node of figma.currentPage.selection) {
+      const shapeType = isValidShapeType(node.type);
+      const textType = isValidTextType(node.type);
+      if (node.type === shapeType) {
+        node.effects = [baseGlow, spreadGlow];
+      }
+      if (node.type === "TEXT") {
+        cloneNode = node.clone();
+        cloneNode.name = "Clone";
+        node.name = "Base";
+        figma.currentPage.appendChild(cloneNode);
+        const group = figma.group([node, cloneNode], figma.currentPage);
+        group.name = "Group Node With Effect";
+        node.effects = [textGlow];
+        cloneNode.x = node.x;
+        cloneNode.y = node.y;
+      }
     }
   }
+
   return null;
 };
